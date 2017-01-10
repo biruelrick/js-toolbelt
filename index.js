ed47f5f9b2c4a37971bc4487ced9dd5c8392abab
Ricardo Biruel <biruel@infodesk.com.br>
2017-01-10 10:57:42 -0200

    activate the 3 news libraries (http, validNumber and validString)

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 module.exports = {
-  HelloWorld: require('./lib/helloworld')
+  HelloWorld: require('./lib/helloworld'),
+  Http: require('./lib/http'),
+  ValidNumber: require('./lib/validNumber'),
+  ValidString: require('./lib/validString')
 };
