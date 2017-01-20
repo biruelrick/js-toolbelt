b13d36cd111aa121e7055e162e05b479659890b8
Ricardo Biruel <biruel@infodesk.com.br>
2017-01-20 15:07:58 -0200

    fixed requires for deprecated http library

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 module.exports = {
   HelloWorld: require('./lib/helloworld'),
-  Http: require('./lib/http'),
+  // Http: require('./lib/http'),
   ValidNumber: require('./lib/validNumber'),
   ValidString: require('./lib/validString'), 
   ValidField: require('./lib/validField')
